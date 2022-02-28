b46dda05bca40783d1f2177c1f2112a19e68b7f5
小文 <yaowenchen@caixin.com>
2022-02-28 17:28:14 +0800

    提交代码

--- 1701Portal/1701Portal/Class/HomePage/ViewController/HomePageViewController.m
+++ 1701Portal/1701Portal/Class/HomePage/ViewController/HomePageViewController.m
@@ -8,7 +8,8 @@
 #import "HomePageViewController.h"
 
 @interface HomePageViewController ()
-
+@property (nonatomic, strong)UIScrollView * scrollview;
+@property (nonatomic, strong)UIView * contentView;
 @end
 
 @implementation HomePageViewController
@@ -17,8 +18,39 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = UIColor.redColor;
+    
+}
+- (void)createUI {
+    [self.view addSubview:self.scrollview];
+    [self.scrollview mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.bottom.mas_equalTo(0);
+    }];
+    [self.scrollview addSubview:self.contentView];
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.mas_equalTo(0);
+        make.width.mas_equalTo(self.scrollview);
+    }];
+    
+    
+    
+    
 }
 
+
+
+
+- (UIScrollView *)scrollview {
+    if (_scrollview == nil) {
+        _scrollview = [UIScrollView new];
+    }
+    return _scrollview;
+}
+-(UIView *)contentView {
+    if (_contentView == nil) {
+        _contentView = [UIView new];
+    }
+    return _contentView;
+}
 /*
 #pragma mark - Navigation
 
